c67f81ef5bc9eba4e360dd2ca95c95b8e3ac7288
lautarog1596 <lautarog1596@gmail.com>
2022-04-26 20:31:57 -0300

    Add express-session and passport to package.json Initialize and require passport and express-session

--- server.js
+++ server.js
@@ -3,8 +3,20 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const session = require('express-session');
+const passport = require('passport');
 
 const app = express();
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.set('view engine', 'pug');
 
