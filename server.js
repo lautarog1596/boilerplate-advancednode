e1873d3472de2edda9b5b04e52a7a9c77d897c46
lautarog1596 <lautarog1596@gmail.com>
2022-04-26 21:01:09 -0300

    Setup database lifecycle functionality

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const passport = require('passport');
 
 const app = express();
 
+app.set('view engine', 'pug');
+
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: true,
@@ -19,29 +21,49 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-passport.serializeUser((user, done) => {
-  done(null, user._id);
-});
+myDB(async client => {
+  const myDataBase = await client.db('database').collection('users');
 
-passport.deserializeUser((id, done) => {
-  myDB.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-    done(null, null);
+  // Be sure to change the title
+  app.route('/').get((req, res) => {
+    //Change the response to render the Pug template
+    res.render('pug', {
+      title: 'Connected to Database',
+      message: 'Please login'
+    });
+  });
+
+  // Serialization and deserialization here...
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+  
+  passport.deserializeUser((id, done) => {
+    myDB.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      done(null, doc);
+    });
+  });
+
+  // Be sure to add this...
+}).catch(e => {
+  app.route('/').get((req, res) => {
+    res.render('pug', { title: e, message: 'Unable to login' });
   });
 });
 
-app.set('view engine', 'pug');
+
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.route('/').get((req, res) => {
-  res.render(process.cwd() + '/views/pug/index', {
-    title: 'Hello', 
-    message: 'Please login'
-  });
-});
+// app.route('/').get((req, res) => {
+//   res.render(process.cwd() + '/views/pug/index', {
+//     title: 'Hello', 
+//     message: 'Please login'
+//   });
+// });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
