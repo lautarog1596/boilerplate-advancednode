2bb13c98a14cb0047cc0795b4cc6948fe6a2be0b
lautarog1596 <lautarog1596@gmail.com>
2022-04-27 18:55:13 -0300

    Add local-passport strategy dependency Added the strategy to the database connection flow

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const ObjectID = require('mongodb').ObjectID;
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const session = require('express-session');
 const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
 const app = express();
 
@@ -43,6 +44,18 @@ myDB(async client => {
       done(null, doc);
     });
   });
+
+  passport.use(new LocalStrategy(
+    function(username, password, done) {
+      myDataBase.findOne({ username: username }, function (err, user) {
+        console.log('User '+ username +' attempted to log in.');
+        if (err) { return done(err); }
+        if (!user) { return done(null, false); }
+        if (password !== user.password) { return done(null, false); }
+        return done(null, user);
+      });
+    }
+  ));
 
   // Be sure to add this...
 }).catch(e => {
