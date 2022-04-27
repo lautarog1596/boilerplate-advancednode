99ee4f877981b7b866c172a0110924f6f5ae3baf
lautarog1596 <lautarog1596@gmail.com>
2022-04-27 19:59:15 -0300

    Add register process for new user

--- server.js
+++ server.js
@@ -31,7 +31,8 @@ myDB(async client => {
     res.render('pug', {
       title: 'Connected to Database',
       message: 'Please login',
-      showLogin: true
+      showLogin: true,
+      showRegistration: true
     });
   });
 
@@ -45,6 +46,37 @@ myDB(async client => {
       done(null, doc);
     });
   });
+
+  app.route('/register')
+  .post((req, res, next) => {
+    myDataBase.findOne({ username: req.body.username }, function(err, user) {
+      if (err) {
+        next(err);
+      } else if (user) {
+        res.redirect('/');
+      } else {
+        myDataBase.insertOne({
+          username: req.body.username,
+          password: req.body.password
+        },
+          (err, doc) => {
+            if (err) {
+              res.redirect('/');
+            } else {
+              // The inserted document is held within
+              // the ops property of the doc
+              next(null, doc.ops[0]);
+            }
+          }
+        )
+      }
+    })
+  },
+    passport.authenticate('local', { failureRedirect: '/' }),
+    (req, res, next) => {
+      res.redirect('/profile');
+    }
+  );
 
   app.route('/login').post(passport.authenticate('local', { failureRedirect: '/'}), 
     (req, res) => {
