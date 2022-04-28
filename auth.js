0554dee6813d470b185b1e7d02701f732ed99dc9
lautarog1596 <lautarog1596@gmail.com>
2022-04-28 11:03:46 -0300

    Empower github authentication to either update existing users or create new ones

--- auth.js
+++ auth.js
@@ -6,7 +6,7 @@ const GitHubStrategy = require('passport-github').Strategy
 require('dotenv').config();
 
 
-module.exports = function (app, myDataBase) {
+module.exports = (app, myDataBase) => {
 
   passport.serializeUser((user, done) => {
     done(null, user._id);
@@ -23,15 +23,39 @@ module.exports = function (app, myDataBase) {
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
     callbackURL: '/auth/github/callback'
   },
-    function(accessToken, refreshToken, profile, cb) {
+    (accessToken, refreshToken, profile, cb) => {
       console.log(profile);
-      //Database logic here with callback containing our user object
+      myDataBase.findOneAndUpdate(
+        { id: profile.id },
+        {
+          $setOnInsert: {
+            id: profile.id,
+            name: profile.displayName || 'John Doe',
+            photo: profile.photos[0].value || '',
+            email: Array.isArray(profile.emails)
+              ? profile.emails[0].value
+              : 'No public email',
+            created_on: new Date(),
+            provider: profile.provider || ''
+          },
+          $set: {
+            last_login: new Date()
+          },
+          $inc: {
+            login_count: 1
+          }
+        },
+        { upsert: true, new: true },
+        (err, doc) => {
+          return cb(null, doc.value);
+        }
+      );
     }
   ));
 
   passport.use(new LocalStrategy(
-    function(username, password, done) {
-      myDataBase.findOne({ username: username }, function (err, user) {
+    (username, password, done) => {
+      myDataBase.findOne({ username: username }, (err, user) => {
         console.log('User '+ username +' attempted to log in.');
         if (err) { return done(err); }
         if (!user) { return done(null, false); }
