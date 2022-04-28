95d84a5127a815bb86cb6feffe285521e3d2b228
lautarog1596 <lautarog1596@gmail.com>
2022-04-28 09:11:50 -0300

    Update routes.js with github authentication

--- routes.js
+++ routes.js
@@ -10,8 +10,17 @@ module.exports = function (app, myDataBase) {
       title: 'Connected to Database',
       message: 'Please login',
       showLogin: true,
-      showRegistration: true
+      showRegistration: true,
+      showSocialAuth: true
     });
+  });
+
+  app.route('/auth/github').get(passport.authenticate('github'));
+
+  app.route('/auth/github/callback').get(passport.authenticate('github', {
+    failureRedirect: '/'
+  }), (req, res) => {
+    res.redirect('/profile');
   });
 
   app.route('/register')
