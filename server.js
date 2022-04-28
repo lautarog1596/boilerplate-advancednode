39b57fdc14868b75c1a2dbc808087b908c9b7ff3
lautarog1596 <lautarog1596@gmail.com>
2022-04-28 18:20:24 -0300

    Authentication with Socket.IO

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const session = require('express-session');
 const passport = require('passport');
+const passportSocketIo = require('passport.socketio');
+const cookieParser = require('cookie-parser');
+
+const MongoStore = require('connect-mongo')(session);
+const URI = process.env.MONGO_URI;
+const store = new MongoStore({ url: URI });
 
 const app = express();
 
@@ -31,6 +37,29 @@ myDB(async client => {
 
   routes(app, myDataBase);
   auth(app, myDataBase);
+
+  function onAuthorizeSuccess(data, accept) {
+    console.log('successful connection to socket.io');
+  
+    accept(null, true);
+  }
+  
+  function onAuthorizeFail(data, message, error, accept) {
+    if (error) throw new Error(message);
+    console.log('failed connection to socket.io:', message);
+    accept(null, false);
+  }
+
+  io.use(
+    passportSocketIo.authorize({
+      cookieParser: cookieParser,
+      key: 'express.sid',
+      secret: process.env.SESSION_SECRET,
+      store: store,
+      success: onAuthorizeSuccess,
+      fail: onAuthorizeFail
+    })
+  );
 
   let currentUsers = 0;
 
